665586e4d353ae764300ef9fdc41645db0a398e3
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-09-26 14:01:37 +0300

    header and navbar, not responsive yet

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.nav-button').click(function () {
+        $('.nav-button').toggleClass('change')
+    })
+})
+
+// document.querySelector('.nav-button').addEventListener('click', function(e) {
+//     // const change = document.createElement('.change')
+//     console.log(e)
+// })
