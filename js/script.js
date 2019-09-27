5f98eb5aa503d11d5a84b815785e008e5960b42c
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2019-09-27 22:14:34 +0300

    footer

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ $(document).ready(function () {
     $('.nav-button').click(function () {
         $('.nav-button').toggleClass('change')
     })
+
+    $(window).scroll(function () {
+        let position = $(this).scrollTop()
+        if (position >= 400) {
+            $('.euratas-nav__brand').addClass('euratas-nav__brand--hidden')
+        } else {
+            $('.euratas-nav__brand').removeClass('euratas-nav__brand--hidden')
+        }
+    })
 })
 
 // document.querySelector('.nav-button').addEventListener('click', function(e) {
